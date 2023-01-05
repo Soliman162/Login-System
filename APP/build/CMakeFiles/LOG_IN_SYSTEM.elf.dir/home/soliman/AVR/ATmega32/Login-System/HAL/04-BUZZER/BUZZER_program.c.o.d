971fe79c884e3bdd61827b02bf430aa655331612
Soliman162 <ahmedsliman215@gmail.com>
2023-01-05 23:50:11 +0200

    review

--- APP/build/CMakeFiles/LOG_IN_SYSTEM.elf.dir/home/soliman/AVR/ATmega32/Login-System/HAL/04-BUZZER/BUZZER_program.c.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/LOG_IN_SYSTEM.elf.dir/home/soliman/AVR/ATmega32/Login-System/HAL/04-BUZZER/BUZZER_program.c.o: \
- /home/soliman/AVR/ATmega32/Login-System/HAL/04-BUZZER/BUZZER_program.c \
- /home/soliman/AVR/ATmega32/Login-System/APP/../LIB/std_types.h \
- /home/soliman/AVR/ATmega32/Login-System/APP/../LIB/BIT_MATH.h \
- /home/soliman/AVR/ATmega32/Login-System/APP/../MCAL/01-DIO/DIO_interface.h \
- /home/soliman/AVR/ATmega32/Login-System/APP/../MCAL/02-Timer1/TIMER1_interface.h \
- /home/soliman/AVR/ATmega32/Login-System/HAL/04-BUZZER/BUZZER_interface.h \
- /home/soliman/AVR/ATmega32/Login-System/HAL/04-BUZZER/BUZZER_config.h
--- APP/build/CMakeFiles/LOG_IN_SYSTEM.elf.dir/home/soliman/AVR/ATmega32/Login-System/MCAL/01-DIO/DIO_program.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/LOG_IN_SYSTEM.elf.dir/home/soliman/AVR/ATmega32/Login-System/MCAL/01-DIO/DIO_program.c.o: \
- /home/soliman/AVR/ATmega32/Login-System/MCAL/01-DIO/DIO_program.c \
- /home/soliman/AVR/ATmega32/Login-System/APP/../LIB/std_types.h \
- /home/soliman/AVR/ATmega32/Login-System/APP/../LIB/BIT_MATH.h \
- /home/soliman/AVR/ATmega32/Login-System/MCAL/01-DIO/DIO_interface.h \
- /home/soliman/AVR/ATmega32/Login-System/MCAL/01-DIO/DIO_private.h
