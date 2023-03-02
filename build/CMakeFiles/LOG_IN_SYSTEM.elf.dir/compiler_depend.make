cd44743a73d15afe9eafe2d657becddf82c5f8ab
Soliman162 <ahmedsliman215@gmail.com>
2023-03-02 19:32:35 +0200

    ignore files

--- build/CMakeFiles/LOG_IN_SYSTEM.elf.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for LOG_IN_SYSTEM.elf.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/remove.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/remove"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/remove.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
